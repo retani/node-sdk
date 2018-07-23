c0dc0aab96b014d3d842397eef14010ec5f7a644
Marco Lüthy <marco@luethy.net>
2018-07-23 11:42:25 +0200

    test: update oauth credential env variables

--- src/rest/oauth.test.ts
+++ src/rest/oauth.test.ts
@@ -4,8 +4,8 @@ import { getNewTokenUsingPasswordGrant } from './oauth'
 describe('getNewTokenUsingPasswordGrant()', () => {
   it('should return a token given valid credentials', async () => {
     const accessToken = await getNewTokenUsingPasswordGrant(
-      process.env.ALLTHINGS_CLIENT_ID as string,
-      process.env.ALLTHINGS_CLIENT_SECRET as string,
+      process.env.ALLTHINGS_OAUTH_CLIENT_ID as string,
+      process.env.ALLTHINGS_OAUTH_CLIENT_SECRET as string,
       process.env.ALLTHINGS_OAUTH_USERNAME as string,
       process.env.ALLTHINGS_OAUTH_PASSWORD as string,
     )
