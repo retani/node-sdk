6107b434aa2e4bad44dad8a765a285938b930128
Moritz Schwörer <mr.mosch@gmail.com>
2018-10-16 09:01:15 +0200

    fix: only accept strings here

--- src/rest/post.ts
+++ src/rest/post.ts
@@ -5,14 +5,14 @@ export type PostResult = Promise<any>
 export type MethodHttpPost = (
   method: string,
   body?: { readonly [key: string]: any },
-  headers?: { readonly [key: string]: any },
+  headers?: { readonly [key: string]: string },
 ) => PostResult
 
 export default async function post(
   request: MethodHttpRequest,
   method: string,
   body: { readonly [key: string]: any },
-  headers: { readonly [key: string]: any },
+  headers: { readonly [key: string]: string },
 ): PostResult {
   return request('post', method, { body, headers })
 }
